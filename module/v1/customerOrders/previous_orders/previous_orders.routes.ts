3519c00331b4587ff89273a83a889f020995393e
Tariq-Monowar <hassainseam@gmail.com>
2026-02-17 15:16:20 +0600

    update

--- module/v1/customerOrders/previous_orders/previous_orders.routes.ts
+++ module/v1/customerOrders/previous_orders/previous_orders.routes.ts
@@ -2,7 +2,22 @@ import express from "express";
 import { verifyUser } from "../../../../middleware/verifyUsers";
 
 import upload from "../../../../config/multer.config";
+import {
+  getPreviousOrders,
+  getSinglePreviousOrder,
+} from "./previous_orders.controllers";
 
 const router = express.Router();
+
+router.get(
+  "/get-all/:customerId",
+  verifyUser("ADMIN", "PARTNER", "EMPLOYEE"),
+  getPreviousOrders,
+);
+router.get(
+  "/get-single/:customerId/:orderId",
+  verifyUser("ADMIN", "PARTNER", "EMPLOYEE"),
+  getSinglePreviousOrder,
+);
 
 export default router;
